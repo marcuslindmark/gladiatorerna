dc734ae58f17cd99e221778053e771f4b765eb05
Marcus Malinda Lindmark <supermarcus.lindmark@gmail.com>
2024-09-15 14:23:09 +0200

    Har programmerat klart V1 av gladiatorerna (utan vapen)

--- gladiatorerna.py
+++ gladiatorerna.py
@@ -8,17 +8,34 @@ colorama.init()
 #print(Fore.GREEN + "Detta är grön text" + Style.RESET_ALL)
 #print(Fore.YELLOW + "Detta är gul text" + Style.RESET_ALL)
 
-# Hälsopoäng
-spelarens_hp = 5
-fiendens_hp = 3
+### VARIABLER I SPELET ###
 
-# Träffchans
+# Övrigt
+strid_pågår = True
+runda = 1
+
+# Hälsopoäng
+spelarens_hp = 10
+fiendens_hp = 6
+
+# Träffchans spelaren
 spelarens_träffchans = 5
+
+# Träffchans fienden
 fiendens_träffchans = 3
 
-# Attacker
-skada_järnhård_näve = 2
-skada_kvick_spark = 1
+### Attacker ###
+
+# Järnhård näve
+lägsta_skada_järnhård_näve = 2
+högsta_skada_järnhård_näve = 3
+modifiering_träffchans_järnhård_näve = -2 
+
+# Kvick spark
+lägsta_skada_kvick_spark = 1
+högsta_skada_kvick_spark = 2
+modifiering_träffchans_kvick_spark = 2 
+
 
 # Att göra: Programmera ett stridssystem utan vapen
 print ("\n\n" + Fore.BLUE + "GLADIATORERNA" + Style.RESET_ALL)
@@ -32,35 +49,101 @@ print ("Publiken som sitter runt omkring er ser förväntansfulla ut.")
 print ("Postemius ser ut att göra sig redo att gå till anfall.")
 print ("Striden kan börja.\n")
 
-print (f"Du har just nu {spelarens_hp} hälsopoäng kvar.")
-print (f"Din fiende har just nu {fiendens_hp} hälsopoäng kvar.")
 
-# Järnhård näve ger 2 skada och -2 träffchans
-# Kvick spark ger 1 skada och +2 träffchans
-spelarens_val = input ("Vill du attackera med din järnhårda " + Fore.GREEN + "näve " + Style.RESET_ALL + "eller med din kvicka " + Fore.YELLOW + "spark? " + Style.RESET_ALL).lower()
+while (strid_pågår):
+    Fore.RED + "Detta är röd text" + Style.RESET_ALL
+    
+    print (Fore.RED + "Ni spelar runda nummer " + Fore.YELLOW + str(runda) + Style.RESET_ALL + ".")
+    runda = runda + 1
+    
+    print (f"\nDu har just nu {spelarens_hp} hälsopoäng kvar.")
+    print (f"Din fiende har just nu {fiendens_hp} hälsopoäng kvar.")
 
-T10 = random.randint(1, 10)
+    # Järnhård näve ger 2 och 4 i skada och -2 träffchans
+    # Kvick spark ger mellan 1 och 3 i skada och +2 träffchans
+    spelarens_val = input ("\nVill du attackera med din järnhårda " + Fore.GREEN + "näve " + Style.RESET_ALL + "eller med din kvicka " + Fore.YELLOW + "spark? " + Style.RESET_ALL).lower()
+
+    
+    T10 = random.randint(1, 10)
+
+    if (spelarens_val == "näve"):
+        print ("Du slår hårt mot din motståndare.") 
+        if (T10 <= spelarens_träffchans + modifiering_träffchans_järnhård_näve):   
+            print ("Och träffar!")
+
+            # Beräknar vad skadan och berättar för spelaren vad som händer
+            skada_järnhård_näve = random.randint(lägsta_skada_järnhård_näve, högsta_skada_järnhård_näve)
+            print (f"Du gör {skada_järnhård_näve} hälsopoäng i skada.")
+
+            fiendens_hp = fiendens_hp - skada_järnhård_näve
+            print (f"Din fiende har nu {fiendens_hp} hälsopoäng kvar.")
+        else:
+            print ("Och missar!")
+    elif (spelarens_val == "spark"):
+        print ("Du skickar iväg en snabb spark.")
+        if (T10 <= spelarens_träffchans + modifiering_träffchans_kvick_spark):
+            print ("Och träffar!")
+
+            # Beräknar vad skadan och berättar för spelaren vad som händer
+            skada_kvick_spark = random.randint(lägsta_skada_kvick_spark, högsta_skada_kvick_spark)
+            print (f"Du gör {skada_kvick_spark} hälsopoäng i skada.")
+
+            fiendens_hp = fiendens_hp - skada_kvick_spark
+            print (f"Din fiende har nu {fiendens_hp} hälsopoäng kvar.")
+        else:
+            print ("Och missar!")        
+
+    ### FIENDEN ATTACKERAR! ###
+
+    # Bestäm vilken attack som fienden väljer
+    fiendens_attacker = ["slag", "spark"]
+    fiendens_val = random.choice(fiendens_attacker)
+        
+    T10 = random.randint(1, 10)
+    
+    # Kontrollerar att fienden inte redan är besegrad
+    if (fiendens_hp > 0):
+        # Om fienden lever körs striden
+        print ("\nNu attackerar din motståndare dig!")
+        if (fiendens_val == "slag"):
+            print ("Din fiende slår hårt mot dig.") 
+            if (T10 <= fiendens_träffchans + modifiering_träffchans_järnhård_näve):   
+                print ("Och träffar!")
+
+                # Beräknar vad skadan och berättar för spelaren vad som händer
+                skada_järnhård_näve = random.randint(lägsta_skada_järnhård_näve, högsta_skada_järnhård_näve)
+                print (f"Din fiende gör {skada_järnhård_näve} hälsopoäng i skada.")
+
+                spelarens_hp = spelarens_hp - skada_järnhård_näve
+                print (f"Du har nu {spelarens_hp} hälsopoäng kvar.")
+            else:
+                print ("Och missar!")
+        elif (fiendens_val == "spark"):
+            print ("Din fiende skickar iväg en snabb spark mot dig.")
+            if (T10 <= fiendens_träffchans + modifiering_träffchans_kvick_spark):
+                print ("Och träffar!")
+
+                # Beräknar vad skadan och berättar för spelaren vad som händer
+                skada_kvick_spark = random.randint(lägsta_skada_kvick_spark, högsta_skada_kvick_spark)
+                print (f"Din fiende gör {skada_kvick_spark} hälsopoäng i skada.")
+
+                spelarens_hp = spelarens_hp - skada_kvick_spark
+                print (f"Du har nu {spelarens_hp} hälsopoäng kvar.")
+            else:
+                print ("Och missar!")
 
 
+    print ("\n")    # Extra radbrytning.
+    
+    # Kontrollerar om det är dags att avsluta striden
+    if (spelarens_hp <= 0 or fiendens_hp <= 0):
+        strid_pågår = False
 
-if (spelarens_val == "näve"):
-    print ("Du slår hårt mot din motståndare.") 
-    if (T10 <= spelarens_träffchans - 2):           # -2 eftersom järnhård näve minskar träffchansen
-        print ("Och träffar!")
-        fiendens_hp = fiendens_hp - skada_järnhård_näve
-        print (f"Postemius har nu {fiendens_hp} hälsopoäng kvar.")
-    else:
-        print ("Och missar!")
-elif (spelarens_val == "spark"):
-    print ("Du skickar iväg en snabb spark.")
-    if (T10 <= spelarens_träffchans + 2):           # +2 eftersom kvick spark ökar träffchansen
-        print ("Och träffar!")
-        fiendens_hp = fiendens_hp - skada_kvick_spark
-        print (f"Postemius har nu {fiendens_hp} hälsopoäng kvar.")
-    else:
-        print ("Och missar!") 
-
-
+# Slut meddelande
+if (spelarens_hp <= 0):
+    print ("Du förlorade striden. Bättre lycka nästa gång.")
+elif (fiendens_hp <= 0):
+    print ("Din fiende förlorade striden. Grattis du vann.")
 
 
 # Avsluta colorama (endast för god praxis)
